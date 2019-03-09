230adcdf499a4dc53cd66c78e2070505c239d4d4
acandael <info@anthonycandaele.com>
2019-03-09 10:51:42 +0100

    Updates

---
+++ precache-manifest.8eb321a78e153389646291872cf0315e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a95be79e04917852778",
+    "url": "/course-directory/static/css/main.c3d5686d.chunk.css"
+  },
+  {
+    "revision": "4a95be79e04917852778",
+    "url": "/course-directory/static/js/main.4a95be79.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "c58b705fddb332981af1582e5699d2c7",
+    "url": "/course-directory/index.html"
+  }
+];
